c697f76fd7c198d646f04cae5cc8d0bd3302a332
Erik <erikpersson0884@gmail.com>
2025-02-13 21:23:19 +0100

    added a add-product popup

---
+++ frontend/src/Components/Inventory/AddProductPopup.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import PopupDiv from "../PopupDiv/PopupDiv";
+
+import { useInventory } from "../../Contexts/InventoryContext";
+import { Product } from "../../Types";
+
+interface AddProductPopupProps {
+    closePopup?: () => void;
+    setShowPopupDiv: React.Dispatch<React.SetStateAction<boolean>>;
+    showPopupDiv: boolean;
+}
+
+const AddProductPopup: React.FC<AddProductPopupProps> = ({ closePopup = () => {}, setShowPopupDiv, showPopupDiv }) => {
+    const { addProduct } = useInventory();
+
+
+    const [newProduct, setNewProduct] = React.useState<Product>(
+        {
+            id: 0,
+            name: "",
+            price: 0,
+            amountInStock: 0,
+            available: false,
+            imageUrl: ""
+        }
+    );
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type, checked } = e.target;
+        setNewProduct({
+            ...newProduct,
+            [name]: type === "checkbox" ? checked : type === "number" ? parseFloat(value) : value
+        });
+    };
+
+    
+
+    const handleAddProduct = () => {
+        addProduct(newProduct);
+        setShowPopupDiv(false);
+    };
+
+    return (
+        <PopupDiv title="Lägg till vara" doAction={handleAddProduct} showPopupDiv={showPopupDiv} setShowPopupDiv={setShowPopupDiv}>
+            <div>
+                <label>Varunamn</label>
+                <input type="text" name="name" value={newProduct.name} onChange={handleInputChange} />
+            </div>
+
+            <div>
+                <label>Pris</label>
+                <input type="number" name="price" value={newProduct.price} onChange={handleInputChange} />
+            </div>
+
+            <div>
+                <label>Antal i lager</label>
+                <input type="number" name="amountInStock" value={newProduct.amountInStock} onChange={handleInputChange} />
+            </div>
+            
+            <div>
+                <label>Finns i lager</label>
+                <input type="checkbox" name="available" checked={newProduct.available} onChange={handleInputChange} />
+            </div>
+
+            <div>
+                <label>Bild URL</label>
+                <input type="text" name="imageUrl" value={newProduct.imageUrl} onChange={handleInputChange} />
+            </div>
+        </PopupDiv>
+    );
+}
+
+export default AddProductPopup;
--- frontend/src/Components/Inventory/InventoryPage.tsx
+++ frontend/src/Components/Inventory/InventoryPage.tsx
@@ -3,18 +3,31 @@ import './InventoryPage.css';
 
 import InventoryItem from './InventoryItem/InventoryItem';
 import { useInventory } from '../../Contexts/InventoryContext';
+import AddProductPopup from './AddProductPopup';
 
 const InventoryPage: React.FC = () => {
     const { products } = useInventory();
+    const [showAddProductPopup, setShowAddProductPopup] = React.useState(false);
     
 
     return (
+        <>
         <ul className='inventory-page noUlFormatting'>    
             {products.map((product) => (
                 <InventoryItem key={product.id} product={product} />
             ))}
+
+            <button className='add-button' onClick={() => setShowAddProductPopup(!showAddProductPopup)}>
+                <img src='/images/add.svg' alt='Add product' />
+                <p>Lägg till vara</p>
+            </button>
         </ul>
+
+        <AddProductPopup showPopupDiv={showAddProductPopup} setShowPopupDiv={setShowAddProductPopup} />
+        </>
     );
 };
+
+
 
 export default InventoryPage;
