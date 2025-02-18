48b4de990fd3118465c770a828d5c2304289022d
Erik <erikpersson0884@gmail.com>
2025-02-18 11:38:12 +0100

    added hamburger menu to header

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -3,21 +3,63 @@ import './Header.css';
 
 import { Link } from 'react-router-dom';
 
+const links = [
+    { to: '/', text: 'Strecka' },
+    { to: '/inventory', text: 'Inventory' },
+    { to: '/balance', text: 'Tillgodo' },
+    { to: '/purchases', text: 'Purchases' },
+]
+
+
 const Header: React.FC = () => {
+
+    const [navOpen, setNavOpen] = React.useState(false);
+
     return (
         <header className="page-header">
-            <Link to="/">
-                <img src="images/prit25.png" alt="logo" height={100} />
-            </Link>
-            
-            {/* <nav> */}
-                <Link to="/">Strecka</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/balance">Tillgodo</Link>
-                <Link to="/purchases">Purchases</Link>
+            <div>
+                <Link to="/">
+                    <img src="images/prit25.png" alt="logo" height={100} />
+                </Link>
 
-            {/* </nav> */}
+                <HeaderNav className='inline-header-nav'/>
+
+                <button className="open-nav-button" onClick={() =>  setNavOpen(!navOpen)}>
+                    <img src="images/menu-icon.svg" alt="menu" height={50} />
+                </button>
+            </div>
+
+            {navOpen &&
+                <>
+                    <hr />
+                    <HeaderNav className='mobile-header-nav' setNavOpen={setNavOpen}/>
+                </> 
+                }
+
+
         </header>
+    );
+};
+
+interface HeaderNavProps {
+    className: string;
+    setNavOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const HeaderNav: React.FC<HeaderNavProps> = ({className, setNavOpen}) => {
+    return (
+        <nav className={'' + className}> 
+        
+            {links.map(link => 
+                <Link 
+                    to={link.to} 
+                    key={link.to} 
+                    onClick={() => setNavOpen && setNavOpen(false)}
+                >
+                    {link.text}
+                </Link>
+            )}
+        </nav>
     );
 };
 
