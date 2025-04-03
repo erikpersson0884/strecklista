5668183b879fb1134a35a9bd64df175ed9132194
Erik <erikpersson0884@gmail.com>
2025-04-03 22:58:12 +0200

    added oliver lindell in the footer credits

--- frontend/src/layouts/footer/Footer.tsx
+++ frontend/src/layouts/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     return (
         <footer className='page-footer'>
             <p>
-                Built by <a href="https://github.com/erikpersson0884/">Erik Persson</a> in 2025 
+                Built by <a href="https://github.com/erikpersson0884/">Göken</a> and <a href="https://github.com/olillin/">Cal</a> in 2025 
             </p>
         </footer>
     );
