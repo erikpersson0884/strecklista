b445fb5e6652d4f6b2d17c1f64fa2510434eac72
Erik <erikpersson0884@gmail.com>
2025-05-07 21:14:30 +0200

    moved jest files into its own directory

--- frontend/jest/jest.config.js
+++ frontend/jest/jest.config.js
@@ -1,12 +1,13 @@
 export default {
+	rootDir: "../",
 	testEnvironment: "jsdom",
-	setupFiles: ["<rootDir>/jest.setup.js"],
+	setupFiles: ["<rootDir>/jest/jest.setup.js"],
 	transform: {
 	  	"^.+\\.tsx?$": "babel-jest",
 	},
 	moduleNameMapper: {
 	  	"\\.(css|less|scss|sass)$": "identity-obj-proxy", // Mock CSS imports
-		"\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/jest.mock.js", // Mock images
+		"\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/jest/jest.mock.js", // Mock images
 		"^@/contexts/(.*)$": "<rootDir>/src/contexts/$1",
  	}
 };
--- frontend/jest/jest.setup.js
+++ frontend/jest/jest.setup.js
@@ -1,4 +1,6 @@
 const { TextEncoder, TextDecoder } = require('util');
+
+// Add to global scope
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-globalThis.__API_BASE__= 'http://localhost:3000/api/v1';
+global.__API_BASE__ = 'http://localhost:3000/api/v1';
