036b17c48113cea90fd6e614b90ac800e8c52d0b
Erik <erikpersson0884@gmail.com>
2025-05-08 20:43:36 +0200

    added balance and log out button to profile page

---
+++ src/pages/profilePage/profilePage.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './profilePage.css';
+
+import { useAuth } from '../../contexts/AuthContext';
+
+const ProfilePage: React.FC = () => {
+    const { currentUser, logout } = useAuth();
+
+    return (
+        <div className='profile-page'>
+            {currentUser ? (
+            <>
+                <div>
+                    <div className='profile-image' style={{backgroundImage: `url(${currentUser?.icon})`}}></div>
+                    <div className='profile-name'>
+                        <h1>{currentUser?.nick}</h1>
+                        <hr />
+                        <p>{currentUser.name}</p>
+                    </div>
+                </div>
+
+                <p>Saldo: {currentUser.balance} kr</p>
+
+                <button className='logout-button' onClick={logout}>Logga ut</button>
+            </>
+            ) : (
+                <h1>Not logged in</h1>
+            )}
+
+        </div>
+    );
+};
+
+export default ProfilePage;
