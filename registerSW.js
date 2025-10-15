363bcd48ea92787fada9bfe272c7437c9c3f570c
Erik <erikpersson0884@gmail.com>
2025-10-15 10:14:00 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/strecklista/sw.js', { scope: '/strecklista/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
