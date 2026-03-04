6cf2f42e47d28ec45e2b04b812487e94f8d1a451
Erik <erikpersson0884@gmail.com>
2026-03-04 02:08:26 +0100

    improved transaction page code structure and also made whole transaction item open its modal instead of just button

--- src/pages/transactionsPage/Filter/Filter.tsx
+++ src/pages/transactionsPage/Filter/Filter.tsx
@@ -6,90 +6,15 @@ import { useUsersContext } from '../../../contexts/UsersContext';
 import { useTransactionsContext } from '../../../contexts/TransactionsContext';
 
 interface FilterProps {
+    isVisible: boolean;
     hideFilters: () => void;
 }
 
-const Filter: React.FC<FilterProps> = ({hideFilters}) => {
-    const { users } = useUsersContext();
-    const { filters, setFilters, resetFilters } = useTransactionsContext();
+const Filter: React.FC<FilterProps> = ({ isVisible, hideFilters }) => {
+    const { resetFilters } = useTransactionsContext();
 
-    const handleTransactionTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const value: TransactionType | 'all' = e.target.value as TransactionType | 'all';
-        setFilters(f => ({ ...f, transactionType: value}));
-    }
-
-    
-    const UserFilter = () => (
-        <div>
-            <label htmlFor='user-filter'>Användare:</label>
-            <select
-                id='user-filter' 
-                onChange={(e) => setFilters((f) => ({ ...f, userId: e.target.value }))}
-                value={String(filters.userId)}
-            >
-                <option value={'all'}>Alla</option>
-                {users.map((user) => (
-                    <option key={user.id} value={user.id}>
-                        {user.nick}
-                    </option>
-                ))}
-            </select>
-        </div>
-    );
-
-    const TypeFilter = () => (
-        <div>
-                <label htmlFor='transaction-type-filter'>Transaktions-typ:</label>
-                <select
-                    id='transaction-type-filter' 
-                    onChange={(e) => handleTransactionTypeChange(e)}
-                    value={filters.transactionType}
-                >
-                    <option value={'all'}>Alla</option>
-                    <option value={'purchase'}>Köp</option>
-                    <option value={'deposit'}>Insättning</option>
-                    <option value={'stockUpdate'}>Lageruppdatering</option>
-                </select>
-            </div>
-    );
-
-    const StartDateFilter = () => (
-        <div>
-                <label htmlFor='start-date-filter'>Från datum:</label>
-                <input
-                    type='date'
-                    id='start-date-filter'
-                    value={filters.startDate ?? ''}
-                    onChange={(e) => setFilters(f => ({ ...f, startDate: e.target.value || null }))}
-                />
-            </div>
-    );
-
-    const EndDateFilter = () => (
-         <div>
-            <label htmlFor='end-date-filter'>Till datum:</label>
-            <input
-                type='date'
-                id='end-date-filter'
-                value={filters.endDate ?? ''}
-                onChange={(e) => setFilters(f => ({ ...f, endDate: e.target.value || null }))}
-            />
-        </div>
-    );
-
-    const DeletedTransactionFilter = () => (
-        <div>
-            <label htmlFor='show-deleted-transactions'>Visa strukna transaktioner:</label>
-            <input
-                type='checkbox'
-                id='show-deleted-transactions'
-                checked={filters.showRemoved}
-                onChange={() => setFilters(f => ({ ...f, showRemoved: !f.showRemoved }))}
-            />
-        </div>
-    );
-
-    return (
+    if (!isVisible) return null;
+    else return (
         <div className='filter-div'>
             <UserFilter />
             <TypeFilter />
@@ -107,5 +32,101 @@ const Filter: React.FC<FilterProps> = ({hideFilters}) => {
         </div>
     )
 };
+
+const UserFilter = () => {
+    const { filters, setFilters } = useTransactionsContext();
+    const { users } = useUsersContext();
+
+    return (
+        <div>
+            <label htmlFor='user-filter'>Användare:</label>
+            <select
+                id='user-filter' 
+                onChange={(e) => setFilters((f) => ({ ...f, userId: e.target.value }))}
+                value={String(filters.userId)}
+            >
+                <option value={'all'}>Alla</option>
+                {users.map((user) => (
+                    <option key={user.id} value={user.id}>
+                        {user.nick}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+const TypeFilter = () => {
+    const { filters, setFilters } = useTransactionsContext();
+
+    const handleTransactionTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value: TransactionType | 'all' = e.target.value as TransactionType | 'all';
+        setFilters(f => ({ ...f, transactionType: value}));
+    }
+    
+    return (
+        <div>
+            <label htmlFor='transaction-type-filter'>Transaktions-typ:</label>
+            <select
+                id='transaction-type-filter' 
+                onChange={(e) => handleTransactionTypeChange(e)}
+                value={filters.transactionType}
+            >
+                <option value={'all'}>Alla</option>
+                <option value={'purchase'}>Köp</option>
+                <option value={'deposit'}>Insättning</option>
+                <option value={'stockUpdate'}>Lageruppdatering</option>
+            </select>
+        </div>
+    )
+}
+    
+
+
+const StartDateFilter = () => {
+    const { filters, setFilters } = useTransactionsContext();
+    return (
+        <div>
+            <label htmlFor='start-date-filter'>Från datum:</label>
+            <input
+                type='date'
+                id='start-date-filter'
+                value={filters.startDate ?? ''}
+                onChange={(e) => setFilters(f => ({ ...f, startDate: e.target.value || null }))}
+            />
+        </div>
+    )
+}
+    
+
+const EndDateFilter = () => {
+    const { filters, setFilters } = useTransactionsContext();
+    return (
+        <div>
+            <label htmlFor='end-date-filter'>Till datum:</label>
+            <input
+                type='date'
+                id='end-date-filter'
+                value={filters.endDate ?? ''}
+                onChange={(e) => setFilters(f => ({ ...f, endDate: e.target.value || null }))}
+            />
+        </div>
+    )
+}
+
+const DeletedTransactionFilter = () => {
+    const { filters, setFilters } = useTransactionsContext();
+    return (
+        <div>
+            <label htmlFor='show-deleted-transactions'>Visa strukna transaktioner:</label>
+            <input
+                type='checkbox'
+                id='show-deleted-transactions'
+                checked={filters.showRemoved}
+                onChange={() => setFilters(f => ({ ...f, showRemoved: !f.showRemoved }))}
+            />
+        </div>
+    )
+}
 
 export default Filter;
--- src/pages/transactionsPage/TransactionsPage.tsx
+++ src/pages/transactionsPage/TransactionsPage.tsx
@@ -12,124 +12,127 @@ import downIcon from '../../assets/images/down.svg';
 import filterIcon from '../../assets/images/filter.svg';
 
 const TransactionsPage: FC = () => {
-    const { 
-        isLoadingTransactions,
-        filteredTransactions, 
-        getNextTransactions, 
-        getPrevTransactions,
-        transactionsPageNumber,
-        setFilters
-    } = useTransactionsContext();
-    const { openModal } = useModalContext();
+    const { isLoadingTransactions } = useTransactionsContext();
     
-
-    if (isLoadingTransactions) return <p>Loading...</p>;
-
     const [ showFilters, setShowFilters ] = useState<boolean>(false);
 
-    const openTransactionPopup = (transaction: ITransaction) => {
-        openModal(<TransactionPopup transaction={transaction}/>);
-    };
-
-    interface TransactionPreviewProps {
-        transaction: ITransaction;
-    }
-
-    const TransactionPreview: FC<TransactionPreviewProps> = ({transaction}) => {
-        let transactionTypeString: string;
-        switch (transaction.type) {
-            case 'purchase': 
-                transactionTypeString = 'Köp';
-                break;
-            case 'deposit':
-                transactionTypeString = 'Insättning';
-                break;
-            case 'stockUpdate':
-                transactionTypeString = 'Lageruppdatering';
-                break;
-            default:
-                transactionTypeString = 'Okänd';
-        }
-
-        let username: string;
-        if (transaction.type === 'purchase'  ||  transaction.type === 'deposit') username = (transaction as Purchase | Deposit).createdFor.nick;
-        else username = transaction.createdBy.nick;
-
-        return (
-            <li className={`transaction-preview list-item ${transaction.removed ? 'removed-transaction' : ''}`}>
-                <div className='transaction-preview-content'>
-                    <div className='list-item__primary'>
-                        <p>{new Date(transaction.createdTime).toISOString().split('T')[0]}</p>
-                        <p>{username}</p>
-                    </div>
-                    <div className='list-item__secondary'>
-                        <p>{transactionTypeString}</p>
-                        { transaction.type === 'purchase' && ( 
-                            <p>{(transaction as Purchase).total}kr</p>
-                        )} 
-                    </div>
-                </div>
-                <button className='open-popup-button' onClick={() => openTransactionPopup(transaction)}>
-                    <img src={downIcon} alt='Expandera' height={10}/>
-                </button>
-            </li>
-        );
-    }
-
-    const TransactionList = () => {
-        if (filteredTransactions.length === 0) return (
-            <p className='no-transactions'>Inga transaktioner hittades.</p>
-        ) 
-        else return (
-            <ul className='transactions-list'>
-                {filteredTransactions.map((transaction: ITransaction) => 
-                    <TransactionPreview key={transaction.id} transaction={transaction} />
-                )}
-            </ul>
-        );
-    };
-
-    const SearchbarAndFilters = () => {
-        return (
-            <div className='search-and-filter'>
-                <input 
-                    type='text' 
-                    placeholder='Sök transaktioner...' 
-                    className='search-bar' 
-                    onChange={(e) =>
-                        setFilters(f => ({ ...f, searchQuery: e.target.value }))
-                    }
-                />
-                <button 
-                    className='open-filters-button' 
-                    onClick={() => setShowFilters(!showFilters)}
-                >
-                    <img src={filterIcon} alt='Filter' height={10}/>
-                </button>
-            </div>
-        )
-    };
-
-    const Pagination = () => {
-        return (
-            <footer className='pagination'>
-                <button disabled={transactionsPageNumber <= 1} className='prev-button' onClick={getPrevTransactions}>&lt;</button>
-                <span className='page-number'>{transactionsPageNumber}</span>
-                <button disabled={filteredTransactions.length == 0} className='next-button' onClick={getNextTransactions}>&gt;</button>
-            </footer>
-        );
-    };
-
-    
-    return (
+    if (isLoadingTransactions) return <p>Loading...</p>;
+    else return (
         <div className='transactions-page page'>
-            <SearchbarAndFilters />
+            <SearchbarAndFilters showFilters={showFilters} setShowFilters={setShowFilters} />
 
-            {showFilters && <Filter hideFilters={() => setShowFilters(false)}/>}
+            {showFilters && <Filter isVisible={showFilters} hideFilters={() => setShowFilters(false)}/>}
             <TransactionList />
             <Pagination />
         </div>
     );
 };
+
+const TransactionList = () => {
+    const { filteredTransactions } = useTransactionsContext();
+    if (filteredTransactions.length === 0) return (
+        <p className='no-transactions'>Inga transaktioner hittades.</p>
+    ) 
+    else return (
+        <ul className='transactions-list'>
+            {filteredTransactions.map((transaction: ITransaction) => 
+                <TransactionPreview key={transaction.id} transaction={transaction} />
+            )}
+        </ul>
+    );
+};
+
+interface SearchbarAndFiltersProps {
+    showFilters: boolean;
+    setShowFilters: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const SearchbarAndFilters: FC<SearchbarAndFiltersProps> = ({ showFilters, setShowFilters}) => {
+    const { setFilters } = useTransactionsContext();
+
+    return (
+        <div className='search-and-filter'>
+            <input 
+                type='text' 
+                placeholder='Sök transaktioner...' 
+                className='search-bar' 
+                onChange={(e) =>
+                    setFilters(f => ({ ...f, searchQuery: e.target.value }))
+                }
+            />
+            <button 
+                className='open-filters-button' 
+                onClick={() => setShowFilters(!showFilters)}
+            >
+                <img src={filterIcon} alt='Filter' height={10}/>
+            </button>
+        </div>
+    )
+};
+
+
+const Pagination = () => {
+    const { 
+        filteredTransactions, 
+        getNextTransactions, 
+        getPrevTransactions,
+        transactionsPageNumber,
+    } = useTransactionsContext();
+    return (
+        <footer className='pagination'>
+            <button disabled={transactionsPageNumber <= 1} className='prev-button' onClick={getPrevTransactions}>&lt;</button>
+            <span className='page-number'>{transactionsPageNumber}</span>
+            <button disabled={filteredTransactions.length == 0} className='next-button' onClick={getNextTransactions}>&gt;</button>
+        </footer>
+    );
+};
+
+interface TransactionPreviewProps {
+    transaction: ITransaction;
+}
+const TransactionPreview: FC<TransactionPreviewProps> = ({transaction}) => {
+    const { openModal } = useModalContext();
+
+    let transactionTypeString: string;
+    switch (transaction.type) {
+        case 'purchase': 
+            transactionTypeString = 'Köp';
+            break;
+        case 'deposit':
+            transactionTypeString = 'Insättning';
+            break;
+        case 'stockUpdate':
+            transactionTypeString = 'Lageruppdatering';
+            break;
+        default:
+            transactionTypeString = 'Okänd';
+    }
+
+    let username: string;
+    if (transaction.type === 'purchase'  ||  transaction.type === 'deposit') username = (transaction as Purchase | Deposit).createdFor.nick;
+    else username = transaction.createdBy.nick;
+
+    return (
+        <li 
+            className={`transaction-preview list-item ${transaction.removed ? 'removed-transaction' : ''}`} 
+            onClick={() => openModal(<TransactionPopup transaction={transaction}/>)}
+        >
+            <div className='transaction-preview-content'>
+                <div className='list-item__primary'>
+                    <p>{new Date(transaction.createdTime).toISOString().split('T')[0]}</p>
+                    <p>{username}</p>
+                </div>
+                <div className='list-item__secondary'>
+                    <p>{transactionTypeString}</p>
+                    { transaction.type === 'purchase' && ( 
+                        <p>{(transaction as Purchase).total}kr</p>
+                    )} 
+                </div>
+            </div>
+            <button className='open-popup-button'>
+                <img src={downIcon} alt='Expandera' height={10}/>
+            </button>
+        </li>
+    );
+}
 
 export default TransactionsPage;
